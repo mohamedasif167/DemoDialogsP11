6060aeab3f49f2b80c1f8b3351c0b86915cf0d46
mohamedasif167 <mohamedasif167@gmail.com>
2017-08-01 10:40:32 +0800

    Initial commit

---
+++ app/src/main/java/asif167/wordpress/com/demodialogsp11/MainActivity.java
@@ -0,0 +1,58 @@
+package asif167.wordpress.com.demodialogsp11;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Tally against the respective action item clicked
+        //  and implement the appropriate action
+        if (item.getItemId() == R.id.itemConfirmation) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Are you sure?")
+                    // Set text for the positive button and the corresponding
+                    //  OnClickListener when it is clicked
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Toast.makeText(MainActivity.this, "You clicked yes",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    // Set text for the negative button and the corresponding
+                    //  OnClickListener when it is clicked
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Toast.makeText(MainActivity.this, "You clicked no",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        } else if (item.getItemId() == R.id.itemSimpleList) {
+        } else if (item.getItemId() == R.id.itemPassPhrase) {
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return true;
+    }
+
+
+}
